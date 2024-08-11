7825f102df61a000d19c75336f221b4fff939bb6
Nagaraja_HM <nagaraja.hm.0001@gmail.com>
2024-08-11 18:50:51 +0200

    vR0.0.1

--- m_WorkspaceUbuntu_pProjectFull_vR0.0.1_GitRelease.sh
+++ m_WorkspaceUbuntu_pProjectFull_vR0.0.1_GitRelease.sh
@@ -1,18 +1,21 @@
 cd ./repo_submodules/SwcServiceStartup
 git add .
 git commit -m "vR0.0.1"
-git tag "vR0.0.1"
+git tag -a "vR0.0.1" -m "vR0.0.1"
 git push
+git push origin vR0.0.1
 
 cd ../../repo_projects/pProjectFull
 git add .
 git commit -m "vR0.0.1"
-git tag "vR0.0.1"
+git tag -a "vR0.0.1" -m "vR0.0.1"
 git push
+git push origin vR0.0.1
 
 cd ../..
 git add .
 git commit -m "vR0.0.1"
-git tag "vR0.0.1"
+git tag -a "vR0.0.1" -m "vR0.0.1"
 git push
+git push origin vR0.0.1
 

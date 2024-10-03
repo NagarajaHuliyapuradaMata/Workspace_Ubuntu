8a98a3aa7173c571dc6a120db5be3a4c75f9a4d3
Nagaraja HULIYAPURADA MATA <nagaraja.huliyapurada-mata@outlook.com>
2024-10-03 10:50:12 +0530

    Update - 202410031100AM

---
+++ repo_submodules/m_git_submodule_push.sh
@@ -0,0 +1,85 @@
+cd ./aapFunctionalCluster_CommunicationManagement
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_Core
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_Cryptography
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_DiagnosticManagement
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_ExecutionManagement
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_Firewall
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_IntrusionDetectionSystemManager
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_LogAndTrace
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_NetworkManagement
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_Persistency
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_PlatformHealthManagement
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_RawDataStream
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_Registry
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_StateManagement
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_TimeSynchronization
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_UpdateAndConfigurationManagement
+git add .
+git commit -m "Initial format"
+git push
+
+cd ../aapFunctionalCluster_VehicleUpdateAndConfigurationManagement
+git add .
+git commit -m "Initial format"
+git push
+

10bd92db307f1fc36f81042bc9aa0a07000d262a
Nagaraja HULIYAPURADA MATA <Nagaraja.Huliyapurada-Mata@outlook.com>
2024-10-22 12:37:22 +0530

    Update - 202410221230PM

---
+++ repo_submodules/m_git_branch_main.sh
@@ -0,0 +1,76 @@
+echo "common"
+cd ./common
+git checkout main
+
+echo "aapFunctionalCluster_CommunicationManagement"
+cd ../aapFunctionalCluster_CommunicationManagement
+git checkout main
+
+echo "aapFunctionalCluster_Core"
+cd ../aapFunctionalCluster_Core
+git checkout main
+
+echo "aapFunctionalCluster_Cryptography"
+cd ../aapFunctionalCluster_Cryptography
+git checkout main
+
+echo "aapFunctionalCluster_DiagnosticManagement"
+cd ../aapFunctionalCluster_DiagnosticManagement
+git checkout main
+
+echo "aapFunctionalCluster_ExecutionManagement"
+cd ../aapFunctionalCluster_ExecutionManagement
+git checkout main
+
+echo "aapFunctionalCluster_Firewall"
+cd ../aapFunctionalCluster_Firewall
+git checkout main
+
+echo "aapFunctionalCluster_IntrusionDetectionSystemManager"
+cd ../aapFunctionalCluster_IntrusionDetectionSystemManager
+git checkout main
+
+echo "aapFunctionalCluster_LogAndTrace"
+cd ../aapFunctionalCluster_LogAndTrace
+git checkout main
+
+echo "aapFunctionalCluster_NetworkManagement"
+cd ../aapFunctionalCluster_NetworkManagement
+git checkout main
+
+echo "aapFunctionalCluster_Persistency"
+cd ../aapFunctionalCluster_Persistency
+git checkout main
+
+echo "aapFunctionalCluster_PlatformHealthManagement"
+cd ../aapFunctionalCluster_PlatformHealthManagement
+git checkout main
+
+echo "aapFunctionalCluster_RawDataStream"
+cd ../aapFunctionalCluster_RawDataStream
+git checkout main
+
+echo "aapFunctionalCluster_Registry"
+cd ../aapFunctionalCluster_Registry
+git checkout main
+
+echo "aapFunctionalCluster_StateManagement"
+cd ../aapFunctionalCluster_StateManagement
+git checkout main
+
+echo "aapFunctionalCluster_TimeSynchronization"
+cd ../aapFunctionalCluster_TimeSynchronization
+git checkout main
+
+echo "aapFunctionalCluster_UpdateAndConfigurationManagement"
+cd ../aapFunctionalCluster_UpdateAndConfigurationManagement
+git checkout main
+
+echo "aapFunctionalCluster_VehicleUpdateAndConfigurationManagement"
+cd ../aapFunctionalCluster_VehicleUpdateAndConfigurationManagement
+git checkout main
+
+echo "pProjectFull"
+cd ../../repo_projects/pProjectFull
+git checkout main
+
